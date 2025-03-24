61b32bcc07f0f7a31fd373be18a68d49ee0bfb3a
Wilson Zhang <z5587836@ad.unsw.edu.au>
2025-03-24 17:48:10 +1100

    Added the order matching function to the LOB

--- order_book.py
+++ order_book.py
@@ -12,34 +12,65 @@ class limitOrderBook:
     def addOrder(self, action, quantity, price, orderId):
         book = self.bids if action == 'buy' else self.asks
         
+        remainingTrades = self.orderMatching(action, quantity, price)
+        if remainingTrades == 0:
+            return
+        
         if price not in book: 
             # book is a dictionary with the keys being the price and the elemnts being the deque
             book[price] = deque()
         
         # Stores it as a tuple which can be retrieved by 0 indexing
-        book[price].append((orderId, quantity))
+        book[price].append((orderId, remainingTrades))
+
+    def orderMatching(self, action, quantity, price):
+        book = self.bids if action == 'sell' else self.asks
+        
+        while quantity > 0:
+            bestPrice = self.getBestBid() if action == 'sell' else self.getBestAsk()
+             
+            if bestPrice is None or (action == 'sell' and bestPrice < price) or (action == 'buy' and bestPrice > price):
+                break
+
+            # Get the values in the 0th index of the array stored under the key's price
+            orderId, openQuantity = book[bestPrice][0]
+
+            tradesMade = min(quantity, openQuantity)            
+            quantity -= tradesMade
+            openQuantity -= tradesMade
+            
+            if openQuantity == 0:
+                book[bestPrice].popleft()
+                if not book[bestPrice]:
+                    del book[bestPrice] # If all trades of the other side fulfilled, remove price from LOB
+            else:
+                book[bestPrice][0] = (orderId, openQuantity)
+        
+        return quantity # Return amount of trades not matched
 
     # Remove the oldest order at a given price (LOB prioritise price and time)
-    def removeOrder(self, action, quantity, price, orderId):
+    def removeOrder(self, action, price):
         book = self.bids if action == 'buy' else self.asks
         if price in book:
             book[price].popleft()
             if not book[price]:  # If no more orders at this price, delete price level
                 del book[price]
     
-    def get_best_bid(self):
+    def getBestBid(self):
         if self.bids:
-            return self.bids.peekitem(0)
+            bestPrice, _ = self.bids.peekitem(0)
+            return bestPrice
         else:
             return None
         
-    def get_best_ask(self):
+    def getBestAsk(self):
         if self.asks:
-            return self.asks.peekitem(0)
+            bestPrice, _ = self.asks.peekitem(0)
+            return bestPrice
         else:
             return None
     
-    def print_order_book(self):
+    def printOrderBook(self):
         """Prints the current state of the order book"""
         print("\nOrder Book:")
         print("Bids:", dict(self.bids))
